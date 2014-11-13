56debbd414d908cc42d5b5ebc946583607aa9c0b
z-a-x <plut.jaka@gmail.com>
2014-11-13 11:28:50 +0100

    Update welcome page-a

--- Study_Coordinator/src/com/example/study_coordinator/WelcomeActivity.java
+++ Study_Coordinator/src/com/example/study_coordinator/WelcomeActivity.java
@@ -17,7 +17,7 @@ public class WelcomeActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.welcome, menu);
-		return true;
+		return true; 
 	}
 
 	@Override
