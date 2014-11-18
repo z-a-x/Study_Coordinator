7275bb9a629457e64387ac8145d46dd8e68b43f0
z-a-x <plut.jaka@gmail.com>
2014-11-18 08:59:26 +0100

    Database

--- Study_Coordinator/src/com/example/study_coordinator/WelcomeActivity.java
+++ Study_Coordinator/src/com/example/study_coordinator/WelcomeActivity.java
@@ -8,7 +8,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
