f40b45cf983725d91928c44aff25c15e96800bcd
Jure Sorn <sornjure@gmail.com>
2015-01-06 23:11:58 +0100

    FragmentGroup komunicira z serverjem
    novi folder baseclasses kjer naj bi bili osnovni razredi kot so user,
    group, event...

--- Study_Coordinator/src/com/example/study_coordinator/DatabaseConnect.java
+++ Study_Coordinator/src/com/example/study_coordinator/DatabaseConnect.java
@@ -1,7 +1,7 @@
 package com.example.study_coordinator;
 
 public class DatabaseConnect {
-	private static String ipAddress = "http://188.230.178.39:80/Study_Coordinator/android_connect/"; 
+	private static String ipAddress = "http://192.168.1.4:80/Study_Coordinator/android_connect/"; 
 	
 	public String getIpAddress() {
 		return ipAddress;
---
+++ Study_Coordinator/src/com/example/study_coordinator/asynctasks/LookUpUsers.java
@@ -0,0 +1,49 @@
+package com.example.study_coordinator.asynctasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.study_coordinator.baseclasses.User;
+
+import android.content.Context;
+
+/**
+ * Example of received JSON: {"users":[{"user_id":"0","user_name":"aaa","user_last_name":"bbb","username":"aaa"}]}
+ */
+public abstract class LookUpUsers extends LookUp {
+
+	public LookUpUsers(Context context) {
+		super(context);
+	}
+
+	@Override
+	protected JSONObject doInBackground(String... params) {
+		return super.doInBackground("getUsers.php");
+	}
+
+	/*
+	 * STATIC FUNCTIONS, FOR USE IN onSuccessfulFetch(JSONObject result) FUNCTION, THAT NEEDS TO BE
+	 * IMPLEMENTED IN EXTENDING CLASS:
+	 */
+
+	protected static List<User> getUsers(JSONObject result) throws JSONException {
+		List<User> users = new ArrayList<User>();
+		
+		JSONArray usersArray = result.getJSONArray("users");
+		for (int i = 0; i < usersArray.length(); i++) {
+			JSONObject userObject = usersArray.getJSONObject(i);
+			int id = userObject.getInt("user_id");
+			String name = userObject.getString("user_name");
+			String lastName = userObject.getString("user_last_name");
+			String userName = userObject.getString("username");
+			User user = new User(id, name, lastName, userName);
+			users.add(user);
+		}
+		return users;
+	}
+
+}
