fc222fc9274bfe28c8063b021aa8999da038df19
Jure Sorn <sornjure@gmail.com>
2015-01-07 20:21:54 +0100

    Baza vrnjena na staro verzijo + user_id = int

--- android_connect/study_coordinator.sql
+++ android_connect/study_coordinator.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Gostitelj: 127.0.0.1
--- Čas nastanka: 05. jan 2015 ob 12.52
+-- Čas nastanka: 18. nov 2014 ob 08.53
 -- Različica strežnika: 5.6.17
 -- Različica PHP: 5.5.12
 
@@ -45,17 +45,14 @@ CREATE TABLE IF NOT EXISTS `comment` (
 
 CREATE TABLE IF NOT EXISTS `event` (
   `event_id` int(11) NOT NULL AUTO_INCREMENT,
-  `geoloc` varchar(100) NOT NULL,
+  `location_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `event_name` varchar(100) NOT NULL,
-  `date` date NOT NULL,
   `time` datetime NOT NULL,
   `description` text NOT NULL,
-  `scope` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`event_id`),
-  KEY `FK_Relationship_2` (`geoloc`),
-  KEY `FK_Relationship_3` (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `scope` enum('private','protected','public') DEFAULT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -64,13 +61,13 @@ CREATE TABLE IF NOT EXISTS `event` (
 --
 
 CREATE TABLE IF NOT EXISTS `event_file` (
-  `event_file_id` varchar(100) NOT NULL,
+  `event_file_id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` int(11) NOT NULL,
   `path_to_file` text NOT NULL,
-  `file_name` char(10) NOT NULL,
+  `file_name` char(100) NOT NULL,
   PRIMARY KEY (`event_file_id`),
   KEY `FK_Relationship_6` (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -81,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `event_file` (
 CREATE TABLE IF NOT EXISTS `friend` (
   `friend_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`friend_id`),
+  PRIMARY KEY (`friend_id`,`user_id`),
   KEY `FK_Relationship_5` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -97,16 +94,7 @@ CREATE TABLE IF NOT EXISTS `groups` (
   `group_avatar` varchar(100) DEFAULT NULL,
   `groups_owner` varchar(100) NOT NULL,
   PRIMARY KEY (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Odloži podatke za tabelo `groups`
---
-
-INSERT INTO `groups` (`group_id`, `group_name`, `group_avatar`, `groups_owner`) VALUES
-(1, 'Biologi', NULL, ''),
-(2, 'Kemiki', NULL, ''),
-(3, 'Matematiki', NULL, '');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -115,10 +103,12 @@ INSERT INTO `groups` (`group_id`, `group_name`, `group_avatar`, `groups_owner`) 
 --
 
 CREATE TABLE IF NOT EXISTS `location` (
-  `geoloc` varchar(100) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  PRIMARY KEY (`geoloc`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `location_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `latitude` float DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  PRIMARY KEY (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -127,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `location` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `user_name` varchar(100) NOT NULL,
   `user_last_name` varchar(100) NOT NULL,
   `username` varchar(20) NOT NULL,
@@ -135,15 +125,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_avatar` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Odloži podatke za tabelo `user`
---
-
-INSERT INTO `user` (`user_id`, `user_name`, `user_last_name`, `username`, `password`, `user_avatar`, `email`) VALUES
-(1, 'Jaka', 'Plut', 'jPlut', '123', NULL, 'jplut@gmail.com'),
-(2, 'Robert', 'Rozina', 'rRozina', '123', NULL, 'rRozina@gmail.com');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -159,14 +141,6 @@ CREATE TABLE IF NOT EXISTS `user_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Odloži podatke za tabelo `user_group`
---
-
-INSERT INTO `user_group` (`group_id`, `user_id`) VALUES
-(2, 1),
-(1, 2);
-
---
 -- Omejitve tabel za povzetek stanja
 --
 
@@ -174,15 +148,8 @@ INSERT INTO `user_group` (`group_id`, `user_id`) VALUES
 -- Omejitve za tabelo `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `FK_Relationship_8` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  ADD CONSTRAINT `FK_Relationship_4` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`);
-
---
--- Omejitve za tabelo `event`
---
-ALTER TABLE `event`
-  ADD CONSTRAINT `FK_Relationship_3` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`),
-  ADD CONSTRAINT `FK_Relationship_2` FOREIGN KEY (`geoloc`) REFERENCES `location` (`geoloc`);
+  ADD CONSTRAINT `FK_Relationship_4` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`),
+  ADD CONSTRAINT `FK_Relationship_8` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
 -- Omejitve za tabelo `event_file`
@@ -200,8 +167,13 @@ ALTER TABLE `friend`
 -- Omejitve za tabelo `user_group`
 --
 ALTER TABLE `user_group`
-  ADD CONSTRAINT `FK_Relationship_7` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  ADD CONSTRAINT `FK_Relationship_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`);
+  ADD CONSTRAINT `FK_Relationship_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`),
+  ADD CONSTRAINT `FK_Relationship_7` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  
+ALTER TABLE `event`
+  ADD CONSTRAINT `FK_Relationship_3` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`),
+  ADD CONSTRAINT `FK_Relationship_2` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`);
+ 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
