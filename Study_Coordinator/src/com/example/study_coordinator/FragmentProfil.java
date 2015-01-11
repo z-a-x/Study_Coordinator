0b5a3f9a9c5126007808b48906684b8b9132cef6
Jure Sorn <sornjure@gmail.com>
2015-01-11 15:40:55 +0100

    Profil ima zdaj drawer menu

--- Study_Coordinator/src/com/example/study_coordinator/FragmentProfil.java
+++ Study_Coordinator/src/com/example/study_coordinator/FragmentProfil.java
@@ -49,110 +49,89 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class FragmentProfil extends FragmentActivity{
-    TextView tvUsername;
-    TextView tvUserName;
-    TextView tvUserLastName;
-    TextView tvemail;
-    Button btEditProfil;
-    FragmentActivity listener;
-    Bitmap bitmap;
-    ImageView profilPicture;
-    ImageView iw;
-    String pathToPicture;
-    
-    public static final String IMAGE_RESOURCE_ID = "iconResourceID";
-    public static final String ITEM_NAME = "itemName";
-    
-   
-	
+public class FragmentProfil extends FragmentActivityWithDrawer {
+	TextView tvUsername;
+	TextView tvUserName;
+	TextView tvUserLastName;
+	TextView tvemail;
+	Button btEditProfil;
+	FragmentActivity listener;
+	Bitmap bitmap;
+	ImageView profilPicture;
+	ImageView iw;
+	String pathToPicture;
+
+	public static final String IMAGE_RESOURCE_ID = "iconResourceID";
+	public static final String ITEM_NAME = "itemName";
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);   
-        setContentView(R.layout.fragment_layout_profil);
-    	
-    	//ivIcon = (ImageView) view.findViewById(R.id.frag_profil_icon);       
-    	//ivProfil = (ImageView) view.findViewById(R.id.frag_profil_profil_image);
-    	//tvItemName = (TextView) view.findViewById(R.id.frag_profil_tv);
-    	
-    	tvUsername = (TextView) findViewById(R.id.frag_profil_username_tv);
-    	tvUserName = (TextView) findViewById(R.id.frag_profil_name_tv);
-    	tvUserLastName = (TextView) findViewById(R.id.frag_profil_surname_tv);          
-    	tvemail = (TextView) findViewById(R.id.frag_profil_email_tv);
-    	//ivIcon.setImageDrawable(view.getResources().getDrawable(getArguments().getInt(IMAGE_RESOURCE_ID)));
-    	//tvItemName.setText(getArguments().getString(ITEM_NAME));
-        
-    	/*
-        MainActivity m = new MainActivity();
-        final String recivedData = m.getS1();
-        String[] parsedData = recivedData.split(" ");
-        System.out.println("Printing parsed data: ");
-        for(int i = 0; i < parsedData.length; i++){
-        	System.out.println(parsedData[i]);
-        }
-        */
-    	SessionManager session = new SessionManager(getApplicationContext());
-    	HashMap<String, String> pref = session.getUserDetails();
-		
-        
-        
-        String userName = session.getMyName();
-        String userLastName = session.getMyLastName();
-        String username = pref.get(SessionManager.KEY_USERNAME);
-        String email = session.getMyEmail();
-        pathToPicture = session.getMyPicture();
-        final String recivedData = "" + userName + " " + userLastName + " " + username + " " + email+" "+pathToPicture;
-          
-        tvemail.setText(tvemail.getText()+ " "+email);
-        tvUsername.setText(tvUsername.getText()+" "+username);
-        tvUserName.setText(tvUserName.getText()+ " "+userName);
-        tvUserLastName.setText(tvUserLastName.getText()+ " "+userLastName);
-        
-		btEditProfil = (Button) findViewById(R.id.frag_profil_edit_bt);		
-    	btEditProfil.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {            	            	
-            	Intent i = new Intent(FragmentProfil.this, ActivityProfilEdit.class);    
-            	i.putExtra("data", recivedData);
-            	startActivity(i);
-            	
-            }
-        });
-    	
-    	//setup custom fonts buttons & title
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_layout_profil);
+
+		setDrawer();
+
+		tvUsername = (TextView) findViewById(R.id.frag_profil_username_tv);
+		tvUserName = (TextView) findViewById(R.id.frag_profil_name_tv);
+		tvUserLastName = (TextView) findViewById(R.id.frag_profil_surname_tv);
+		tvemail = (TextView) findViewById(R.id.frag_profil_email_tv);
+
+		SessionManager session = new SessionManager(getApplicationContext());
+		HashMap<String, String> pref = session.getUserDetails();
+
+		String userName = session.getMyName();
+		String userLastName = session.getMyLastName();
+		String username = pref.get(SessionManager.KEY_USERNAME);
+		String email = session.getMyEmail();
+		pathToPicture = session.getMyPicture();
+		final String recivedData = "" + userName + " " + userLastName + " " + username + " " + email + " "
+				+ pathToPicture;
+
+		tvemail.setText(tvemail.getText() + " " + email);
+		tvUsername.setText(tvUsername.getText() + " " + username);
+		tvUserName.setText(tvUserName.getText() + " " + userName);
+		tvUserLastName.setText(tvUserLastName.getText() + " " + userLastName);
+
+		btEditProfil = (Button) findViewById(R.id.frag_profil_edit_bt);
+		btEditProfil.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(FragmentProfil.this, ActivityProfilEdit.class);
+				i.putExtra("data", recivedData);
+				startActivity(i);
+
+			}
+		});
+
+		// setup custom fonts buttons & title
 		/*
-    	Typeface custom_font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto/Roboto-Thin.ttf");					
-		tvUserName.setTypeface(custom_font);
-		tvUserLastName.setTypeface(custom_font);
-		tvUsername.setTypeface(custom_font);
-		tvemail.setTypeface(custom_font);
+		 * Typeface custom_font = Typeface.createFromAsset(getActivity().getAssets(),
+		 * "fonts/Roboto/Roboto-Thin.ttf"); tvUserName.setTypeface(custom_font);
+		 * tvUserLastName.setTypeface(custom_font); tvUsername.setTypeface(custom_font);
+		 * tvemail.setTypeface(custom_font);
 		 */
-		//Lookup uporabljamo samo zaradi asynctaska pr loudanju slike
+		// Lookup uporabljamo samo zaradi asynctaska pr loudanju slike
 		LookUp userDetailsFetcher = new LookUpUserDetails(getApplicationContext()) {
 			@Override
 			public void onSuccessfulFetch(JSONObject result) throws JSONException {
-				
-					if(pathToPicture == null || pathToPicture.equals("null") || pathToPicture.equals("NULL")){
-						System.out.println("NI SLIKE");
-						profilPicture = (ImageView) findViewById(R.id.frag_profil_image);
-						profilPicture.setImageResource(R.drawable.test);
-					}
-					else{
-						System.out.println("SLIKAAAAAAAAA "+pathToPicture);
-						DatabaseConnect dc = new DatabaseConnect();
-						new DownloadImageTask((ImageView) findViewById(R.id.frag_profil_image)).execute(dc.getIpAddress()+pathToPicture);
-					}
+
+				if (pathToPicture == null || pathToPicture.equals("null") || pathToPicture.equals("NULL")) {
+					System.out.println("NI SLIKE");
+					profilPicture = (ImageView) findViewById(R.id.frag_profil_image);
+					profilPicture.setImageResource(R.drawable.test);
+				} else {
+					System.out.println("SLIKAAAAAAAAA " + pathToPicture);
+					DatabaseConnect dc = new DatabaseConnect();
+					new DownloadImageTask((ImageView) findViewById(R.id.frag_profil_image)).execute(dc
+							.getIpAddress() + pathToPicture);
 				}
+			}
 		};
 		final String TEST_QUERY = "4";
-		System.out.println("SSSSSSSSSSSSSSS JE "+TEST_QUERY);
+		System.out.println("SSSSSSSSSSSSSSS JE " + TEST_QUERY);
 		userDetailsFetcher.execute("user_id", TEST_QUERY);
-		
-          //new LoadImage().execute("http://http://193.2.179.235:80/android_connect/images/jakaProfil.jpg");          
-          
-    }
-    
-	    
-   
-    
-}
 
+		// new LoadImage().execute("http://http://193.2.179.235:80/android_connect/images/jakaProfil.jpg");
+
+	}
+
+}
